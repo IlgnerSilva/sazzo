32d2ae5a13d423e51420528d41d74b60ac4680d3
sentinex.developer <sentinex.developer@gmail.com>
2025-06-26 20:35:36 +0000

    feat: Integrate better-auth for authentication

---
+++ src/lib/better-auth/auth-client.ts
@@ -0,0 +1,32 @@
+import { createAuthClient } from "better-auth/react";
+import type { auth } from "./auth.ts";
+import {
+    inferAdditionalFields,
+    twoFactorClient,
+    usernameClient,
+    anonymousClient,
+    phoneNumberClient,
+    magicLinkClient,
+    emailOTPClient,
+    apiKeyClient,
+    adminClient,
+    organizationClient,
+    multiSessionClient,
+} from "better-auth/client/plugins";
+
+export const authClient = createAuthClient({
+    baseURL: "http://localhost:3000",
+    plugins: [
+        inferAdditionalFields<typeof auth>(),
+        twoFactorClient(),
+        usernameClient(),
+        anonymousClient(),
+        phoneNumberClient(),
+        magicLinkClient(),
+        emailOTPClient(),
+        apiKeyClient(),
+        adminClient(),
+        organizationClient(),
+        multiSessionClient(),
+    ],
+});
---
+++ src/lib/better-auth/auth.ts
@@ -0,0 +1,50 @@
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/drizzle";
+import {
+	twoFactor,
+	username,
+	anonymous,
+	phoneNumber,
+	magicLink,
+	emailOTP,
+	apiKey,
+	admin,
+	organization,
+	bearer,
+	multiSession,
+	jwt,
+	openAPI,
+} from "better-auth/plugins";
+import { nextCookies } from "better-auth/next-js";
+import { betterAuth } from "better-auth";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	appName: "sazzo",
+	plugins: [
+		openAPI(),
+		jwt(),
+		multiSession(),
+		bearer(),
+		organization(),
+		admin(),
+		apiKey(),
+		emailOTP({
+			async sendVerificationOTP({ email, otp, type }, request) {
+				// Send email with OTP
+			},
+		}),
+		magicLink({
+			sendMagicLink({ email, token, url }, request) {
+				// Send email with magic link
+			},
+		}),
+		phoneNumber(),
+		anonymous(),
+		username(),
+		twoFactor(),
+		nextCookies(),
+	],
+});

028e3d01d12b0e974a4c60e3c708005382c1948f
sentinex.developer <sentinex.developer@gmail.com>
2025-10-01 12:56:03 +0000

    refactor: Remove casos de uso de autenticação
    Remove os casos de uso que foram movidos para serviços. Simplifica a estrutura.

--- src/core/application/use-cases/index.ts
+++
@@ -1,6 +0,0 @@
-export { DisabledTwoFactorUseCase } from "./DisabledTwoFactor.use-case";
-export { EnableTwoFactorUseCase } from "./EnableTwoFactor.use-case";
-export { GetSessionUseCase } from "./GetSession.use-case";
-export { SendOTPTwoFactorUseCase } from "./SendOTPTwoFactor.use-case";
-export { SignInEmailUseCase } from "./SignInEmail.use-case";
-export { VerifyTwoFactorUseCase } from "./VerifyTwoFactor.use-case";
